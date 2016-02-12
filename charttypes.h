5cdd402c1c61ef96aeaf1c74e9ab5a7d601ab07b
Georg <s.g.b@gmx.de>
2016-02-12 21:03:46 +0100

    some code cleanup

---
+++ charttypes.h
@@ -0,0 +1,12 @@
+#ifndef CHARTTYPES_H
+#define CHARTTYPES_H
+
+enum chartTypeTankStatistics
+{
+    chartTypeConsumptionOf100,
+    chartTypeCostsOf100,
+    chartTypeOilPrice
+};
+
+#endif // CHARTTYPES_H
+
